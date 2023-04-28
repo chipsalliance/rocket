79faa733fdfacae266f660383b9764e993477a76
Yanqi Yang <shxxyyq0000@qq.com>
2023-04-28 19:54:50 +0800

    new method to match io in DUT

--- cosim/elaborate/src/DUT.scala
+++ cosim/elaborate/src/DUT.scala
@@ -104,75 +104,66 @@ class DUT(p: Parameters) extends Module {
     // instantiate the LazyModule
     Module(ldut.module)
   ).filterNot(_._2.isInstanceOf[Aggregate]).foreach { case (name, ele) =>
-    if (!(name == "clock" || name == "reset")) {
-      if( name== "nmi_rnmi" ) ele := nmi.rnmi
-      else if (name== "nmi_rnmi_interrupt_vector") ele := nmi.rnmi_interrupt_vector
-      else if (name== "nmi_rnmi_exception_vector") ele := nmi.rnmi_exception_vector
-      else if (name== "intIn_0_0") ele := intIn
-      else if (name== "resetVector") ele := resetVector
-      else if (name== "wfiOut_0_0") wfi := ele
-      else if (name== "ceaseOut_0_0") cease := ele
-      else if (name== "haltOut_0_0") halt := ele
+      name match {
+        case "clock" => {}
+        case "reset" => {}
+        case "nmi_rnmi" => ele := nmi.rnmi
+        case "nmi_rnmi_interrupt_vector" => ele := nmi.rnmi_interrupt_vector
+        case "nmi_rnmi_exception_vector" => ele := nmi.rnmi_exception_vector
+        case "intIn_0_0" => ele := intIn
+        case "resetVector" => ele := resetVector
+        case "wfiOut_0_0" => wfi := ele
+        case "ceaseOut_0_0" => cease := ele
+        case "haltOut_0_0" => halt := ele
 
-      else if (name== "memory_0_a_ready") ele := memory_0_a.ready
-      else if (name== "memory_0_a_valid") memory_0_a.valid := ele
-      else if (name== "memory_0_a_bits_opcode") memory_0_a.bits.opcode  := ele
-      else if (name== "memory_0_a_bits_param")  memory_0_a.bits.param   := ele
-      else if (name== "memory_0_a_bits_size")   memory_0_a.bits.size    := ele
-      else if (name== "memory_0_a_bits_source") memory_0_a.bits.source   := ele
-      else if (name== "memory_0_a_bits_address") memory_0_a.bits.address := ele
-      else if (name== "memory_0_a_bits_mask") memory_0_a.bits.mask    := ele
-      else if (name== "memory_0_a_bits_data") memory_0_a.bits.data    := ele
-      else if (name== "memory_0_a_bits_corrupt") memory_0_a.bits.corrupt := ele
+        case "memory_0_a_ready" => ele := memory_0_a.ready
+        case "memory_0_a_valid" => memory_0_a.valid := ele
+        case "memory_0_a_bits_opcode" => memory_0_a.bits.opcode := ele
+        case "memory_0_a_bits_param" => memory_0_a.bits.param := ele
+        case "memory_0_a_bits_size" => memory_0_a.bits.size := ele
+        case "memory_0_a_bits_source" => memory_0_a.bits.source := ele
+        case "memory_0_a_bits_address" => memory_0_a.bits.address := ele
+        case "memory_0_a_bits_mask" => memory_0_a.bits.mask := ele
+        case "memory_0_a_bits_data" => memory_0_a.bits.data := ele
+        case "memory_0_a_bits_corrupt" => memory_0_a.bits.corrupt := ele
 
-      else if (name == "memory_0_c_ready")        ele := memory_0_c.ready
-      else if (name == "memory_0_c_valid")        memory_0_c.valid := ele
-      else if (name == "memory_0_c_bits_opcode")  memory_0_c.bits.opcode := ele
-      else if (name == "memory_0_c_bits_param")   memory_0_c.bits.param := ele
-      else if (name == "memory_0_c_bits_size")    memory_0_c.bits.size := ele
-      else if (name == "memory_0_c_bits_source")  memory_0_c.bits.source := ele
-      else if (name == "memory_0_c_bits_address") memory_0_c.bits.address := ele
-      else if (name == "memory_0_c_bits_data")    memory_0_c.bits.data := ele
-      else if (name == "memory_0_c_bits_corrupt") memory_0_c.bits.corrupt := ele
+        case "memory_0_c_ready" => ele := memory_0_c.ready
+        case "memory_0_c_valid" => memory_0_c.valid := ele
+        case "memory_0_c_bits_opcode" => memory_0_c.bits.opcode := ele
+        case "memory_0_c_bits_param" => memory_0_c.bits.param := ele
+        case "memory_0_c_bits_size" => memory_0_c.bits.size := ele
+        case "memory_0_c_bits_source" => memory_0_c.bits.source := ele
+        case "memory_0_c_bits_address" => memory_0_c.bits.address := ele
+        case "memory_0_c_bits_data" => memory_0_c.bits.data := ele
+        case "memory_0_c_bits_corrupt" => memory_0_c.bits.corrupt := ele
 
-      else if (name == "memory_0_e_ready") ele := memory_0_e.ready
-      else if (name == "memory_0_e_valid")        memory_0_e.valid := ele
-      else if (name == "memory_0_e_bits_sink") memory_0_e.bits.sink := ele
+        case "memory_0_e_ready" => ele := memory_0_e.ready
+        case "memory_0_e_valid" => memory_0_e.valid := ele
+        case "memory_0_e_bits_sink" => memory_0_e.bits.sink := ele
 
-      else if (name == "memory_0_b_ready")        memory_0_b.ready := ele
-      else if (name == "memory_0_b_valid")        ele := memory_0_b.valid
-      else if (name == "memory_0_b_bits_opcode")  ele := memory_0_b.bits.opcode
-      else if (name == "memory_0_b_bits_param")   ele := memory_0_b.bits.param
-      else if (name == "memory_0_b_bits_size")    ele := memory_0_b.bits.size
-      else if (name == "memory_0_b_bits_source")  ele := memory_0_b.bits.source
-      else if (name == "memory_0_b_bits_address") ele := memory_0_b.bits.address
-      else if (name == "memory_0_b_bits_mask")    ele := memory_0_b.bits.mask
-      else if (name == "memory_0_b_bits_data")    ele := memory_0_b.bits.data
-      else if (name == "memory_0_b_bits_corrupt") ele := memory_0_b.bits.corrupt
+        case "memory_0_b_ready" => memory_0_b.ready := ele
+        case "memory_0_b_valid" => ele := memory_0_b.valid
+        case "memory_0_b_bits_opcode" => ele := memory_0_b.bits.opcode
+        case "memory_0_b_bits_param" => ele := memory_0_b.bits.param
+        case "memory_0_b_bits_size" => ele := memory_0_b.bits.size
+        case "memory_0_b_bits_source" => ele := memory_0_b.bits.source
+        case "memory_0_b_bits_address" => ele := memory_0_b.bits.address
+        case "memory_0_b_bits_mask" => ele := memory_0_b.bits.mask
+        case "memory_0_b_bits_data" => ele := memory_0_b.bits.data
+        case "memory_0_b_bits_corrupt" => ele := memory_0_b.bits.corrupt
 
-      else if (name == "memory_0_d_ready")        memory_0_d.ready := ele
-      else if (name == "memory_0_d_valid")        ele := memory_0_d.valid
-      else if (name == "memory_0_d_bits_opcode")  ele := memory_0_d.bits.opcode
-      else if (name == "memory_0_d_bits_param")   ele := memory_0_d.bits.param
-      else if (name == "memory_0_d_bits_size")    ele := memory_0_d.bits.size
-      else if (name == "memory_0_d_bits_source")  ele := memory_0_d.bits.source
-      else if (name == "memory_0_d_bits_data")    ele := memory_0_d.bits.data
-      else if (name == "memory_0_d_bits_corrupt") ele := memory_0_d.bits.corrupt
-      else if (name == "memory_0_d_bits_denied") ele := memory_0_d.bits.denied
-      else if (name == "memory_0_d_bits_sink") ele := memory_0_d.bits.sink
+        case "memory_0_d_ready" => memory_0_d.ready := ele
+        case "memory_0_d_valid" => ele := memory_0_d.valid
+        case "memory_0_d_bits_opcode" => ele := memory_0_d.bits.opcode
+        case "memory_0_d_bits_param" => ele := memory_0_d.bits.param
+        case "memory_0_d_bits_size" => ele := memory_0_d.bits.size
+        case "memory_0_d_bits_source" => ele := memory_0_d.bits.source
+        case "memory_0_d_bits_data" => ele := memory_0_d.bits.data
+        case "memory_0_d_bits_corrupt" => ele := memory_0_d.bits.corrupt
+        case "memory_0_d_bits_denied" => ele := memory_0_d.bits.denied
+        case "memory_0_d_bits_sink" => ele := memory_0_d.bits.sink
 
-      else{
-        chisel3.experimental.DataMirror.directionOf(ele) match {
-          case ActualDirection.Output =>
-            val io = IO(Output(chiselTypeOf(ele))).suggestName(name)
-            println(s"output $name")
-            io := ele
-          case ActualDirection.Input =>
-            val io = IO(Input(chiselTypeOf(ele))).suggestName(name)
-            println(s"input $name")
-            ele := io
-        }
+        case _ => sys.error(s"can't find $name in DUT")
       }
-    }
   }
 }
