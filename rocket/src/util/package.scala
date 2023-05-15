6fe9f94be46b4c6e316ec8825dae6fedd9ec76c8
Takehana <cowpowermax@pm.me>
2023-05-15 20:45:59 +0800

    TLB: refactor `TLSlaveParameters` related things

--- rocket/src/util/package.scala
+++ rocket/src/util/package.scala
@@ -21,4 +21,14 @@ package object util {
     barrier.io.x := in
     barrier.io.y
   }
+
+  def bitIndexes(x: BigInt, tail: Seq[Int] = Nil): Seq[Int] = {
+    require (x >= 0)
+    if (x == 0) {
+      tail.reverse
+    } else {
+      val lowest = x.lowestSetBit
+      bitIndexes(x.clearBit(lowest), lowest +: tail)
+    }
+  }
 }
